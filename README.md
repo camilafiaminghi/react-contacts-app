f216974e2d9def361a2f751fe1be84e30042a158
camilafiaminghi <camila.fiaminghi@gmail.com>
2018-10-11 11:40:16 -0300

    Add more info contacts list

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 # Project Setup
 - clone the Project - git clone https://github.com/camilafiaminghi/react-contacts-app.git
 - install the dependencies - npm install
+
+# This application has a server dependency
+- clone the Project - git clone https://github.com/udacity/reactnd-contacts-server2.git
+- install the dependencies - npm install
--- src/ContactsList.js
+++ src/ContactsList.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 class ContactList extends Component {
   render() {
     const contacts = this.props.contacts;
-    const contactsList = contacts.map(contact => (<li key={contact.name}>{contact.name}</li>))
+    const contactsList = contacts.map(contact => (
+			<li key={contact.id} className='contact-list-item'>
+				<div
+					className='contact-avatar'
+					style={{
+						backgroundImage: `url(${contact.avatarURL})`
+					}}>
+				</div>
+				<div className='contact-details'>
+					<p>{contact.name}</p>
+					<p>{contact.handle}</p>
+				</div>
+				<button className='contact-remove'>Remove</button>
+			</li>
+    ));
     return (
       <ol>{contactsList}</ol>
     )
