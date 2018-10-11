3bd0ad20c44aa16e957ccbbef7e732992b4db77e
camilafiaminghi <camila.fiaminghi@gmail.com>
2018-10-11 11:54:50 -0300

    Move default contacts into App component as state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
 import ContactsList from './ContactsList';
 
-const contacts = [
-  {
-    id: 'tyler',
-    name: 'Tyler McGinnis',
-    handle: '@tylermcginnis',
-    avatarURL: 'http://localhost:5001/tyler.jpg'
-  },
-  {
-    id: 'karen',
-    name: 'Karen Isgrigg',
-    handle: '@karen_isgrigg',
-    avatarURL: 'http://localhost:5001/karen.jpg'
-  },
-  {
-    id: 'richard',
-    name: 'Richard Kalehoff',
-    handle: '@richardkalehoff',
-    avatarURL: 'http://localhost:5001/richard.jpg'
-  },
-]
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: '@tylermcginnis',
+        avatarURL: 'http://localhost:5001/tyler.jpg'
+      },
+      {
+        id: 'karen',
+        name: 'Karen Isgrigg',
+        handle: '@karen_isgrigg',
+        avatarURL: 'http://localhost:5001/karen.jpg'
+      },
+      {
+        id: 'richard',
+        name: 'Richard Kalehoff',
+        handle: '@richardkalehoff',
+        avatarURL: 'http://localhost:5001/richard.jpg'
+      },
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <ContactsList contacts={contacts}/>
+        <ContactsList contacts={this.state.contacts}/>
       </div>
     );
   }
