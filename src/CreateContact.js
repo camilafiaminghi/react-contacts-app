59a9c4405c7c15655c8db46aca22a0f9e020e31d
camilafiaminghi <camila.fiaminghi@gmail.com>
2018-10-21 16:57:59 -0300

    Add form to create new contacts

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
   render() {
     return (
-      <div>Create Contact</div>
+      <div>
+        <Link
+          className="close-create-contact"
+          to="/" />
+
+        <form className="create-contact-form">
+          <ImageInput
+            className="create-contact-avatar-input"
+            name="AvatarURL"
+            maxHeight={64} />
+          <div className="create-contact-details">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name" />
+            <input
+              type="text"
+              name="handle"
+              placeholder="Handle" />
+            <button>Add Contact</button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
